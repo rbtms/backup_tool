8dc0e69ae6f301768031c7699f24647c21e064a8
Alvaro Fernandez <nishinishi9999@protonmail.com>
2025-02-09 14:14:53 +0100

    Added documentation.

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 def print_directory_tree(d, prefix=''):
-    """Recursively prints a tree-like structure of a dictionary"""
+    """
+        Recursively prints a tree-like structure of a dictionary
+        eg. root {
+            dir_1 {
+                file_1 {}
+                file_2 []
+            }
+            dir_2 {}
+            file_3 {}
+        }
+    """
     ansi_blue = '\033[1;94m'
     ansi_red = '\033[1;91m'
     ansi_reset = '\033[0m'
@@ -28,9 +38,9 @@ def ask_for_confirmation(question):
     """Ask a prompt to the user"""
     response = input(f'{question} (y/n) ')
 
-    if response == 'n' or response == 'N':
+    if response in ('n', 'N'):
         return False
-    elif response == 'y' or response == 'Y':
+    elif response in ('y', 'Y'):
         return True
     else:
         return ask_for_confirmation(question)
